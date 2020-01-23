c2fff0160bc2dcad0d7678790a4ff139b8fc1b81
Sander Cokart <cokart32@gmail.com>
2020-01-23 21:44:21 +0100

    added messages. including error messages and success messages.

--- assets/js/contexts/TodoContext.js
+++ assets/js/contexts/TodoContext.js
@@ -52,16 +52,23 @@ class TodoContextProvider extends React.Component {
     updateTodo(data) {
         axios.put('/api/todo/update/' + data.id, data)
             .then(response => {
-                let todos = [...this.state.todos];
-                let todo = todos.find(todo => {
-                    return todo.id === data.id;
-                });
+                if (response.data.message.level === 'error') {
+                    this.setState({
+                        message: response.data.message,
+                    });
+                } else {
+                    let todos = [...this.state.todos];
+                    let todo = todos.find(todo => {
+                        return todo.id === data.id;
+                    });
 
-                todo.name = data.name;
+                    todo.name = data.name;
 
-                this.setState({
-                    todos: todos,
-                });
+                    this.setState({
+                        todos: todos,
+                        message: response.data.message,
+                    });
+                }
             }).catch(error => {
             console.error(error);
         });
@@ -71,17 +78,24 @@ class TodoContextProvider extends React.Component {
     deleteTodo(data) {
         axios.delete('/api/todo/delete/' + data.id)
             .then(response => {
-                //message
-                let todos = [...this.state.todos];
-                let todo = todos.find(todo => {
-                    return todo.id === data.id;
-                });
+                if (response.data.message.level === 'error') {
+                    this.setState({
+                        message: response.data.message,
+                    });
+                } else {
+                    //message
+                    let todos = [...this.state.todos];
+                    let todo = todos.find(todo => {
+                        return todo.id === data.id;
+                    });
 
-                todos.splice(todos.indexOf(todo), 1);
+                    todos.splice(todos.indexOf(todo), 1);
 
-                this.setState({
-                    todos: todos,
-                });
+                    this.setState({
+                        todos: todos,
+                        message: response.data.message
+                    });
+                }
             }).catch(error => {
             console.error(error);
         });
